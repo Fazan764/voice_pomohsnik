80eae9034b86b13e5f088efbecdccb66e9b17f7a
Fazan764 <130281325+Fazan764@users.noreply.github.com>
2023-04-28 11:26:20 +0300

    Add files via upload

--- game.py
+++ game.py
@@ -1,21 +1,109 @@
 import random
-
+from datetime import datetime
+import time
 
 
 def guess_chislo():
     point = 0
     chislo = random.randint(1,5)
-    while True:
-        playear = int(input('Введите число от 1 до 5 - '))
-        if chislo == playear:
-            print('Вы угадали число')
-            break
-        if chislo >= playear:
-            print('Загаданое число больше')
-        if chislo <= playear:
-            print('Загаданое число меньше')
-        point = point + 1
-    print(f'число было {chislo}, ляпахов дать игроку - {point}')
-   
+    try:
+        while True:
+            playear = int(input('Введите число от 1 до 5 - '))
+            if chislo == playear:
+                print('Вы угадали число')
+                print(f'число было {chislo}, ляпахов дать игроку - {point}')
+                break
+            if chislo >= playear:
+                print('Загаданое число больше')
+            if chislo <= playear:
+                print('Загаданое число меньше')
+            point = point + 1
+            
+    except ValueError:
+        print('Вводить только числа')
+    except RuntimeError:
+        print('Вводить только числа')
+
+
+def matematica_game():
+    tru = 0
+    done_time = []
     
-guess_chislo()
+    while tru != 6:
+        time_start = datetime.now()
+        one_chislo = random.randint(10,100)
+        two_chislo = random.randint(10,100)
+        final_chislo = one_chislo + two_chislo
+   
+        try:
+            print(f'{one_chislo} + {two_chislo}')
+            player_chislo = int(input('Ответ - '))
+            
+            
+            
+         
+            while player_chislo != final_chislo:
+                if player_chislo != final_chislo:
+                    tru = tru - 1
+                print(f'{one_chislo} + {two_chislo}')
+                player_chislo = int(input('Ответ - '))
+            
+
+            if player_chislo == final_chislo:
+                print('Правильно!')
+                tru = tru + 1
+                time_final = datetime.now()
+                sum_time = (time_final - time_start ).seconds 
+                done_time.append(sum_time)
+            
+            
+        except ValueError:
+            print('ВЫ ВВЕЛИ НЕКОРРЕКТНЫЙ ОТВЕТ')
+            tru = tru - 1
+           
+
+   
+    summa = sum(done_time)
+    dlina = len(done_time)
+    min_moment = min(done_time)
+    max_moment = max(done_time)
+    average = summa / dlina
+    print(f'Самое долгое время решения - {max_moment}')
+    print(f'Самое быстрое время решения - {min_moment}')
+    print(f'Ваше среднее время решения - {average}')
+
+
+def game_speed():
+    simbol_pack = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
+    tru_speed = 0
+    done_time_speed = []
+        
+    while tru_speed != 10:
+        random_simbol = random.randint(0,25)
+        simbol = simbol_pack[random_simbol]
+        time_start_speed = datetime.now()
+        print(f'СИМВОЛ - {simbol}')
+        player_chislo_speed = (input(f'Быстро введите символ - '))
+        
+        random_simbol = random.randint(1,26)
+        while player_chislo_speed != simbol:
+            player_chislo_speed = (input(f'Быстро введите символ - '))
+        if player_chislo_speed == simbol:
+            print('Следующая')
+            tru_speed = tru_speed + 1
+            time_final_speed = datetime.now()
+            sum_time_speed = ( time_final_speed - time_start_speed ).seconds
+            done_time_speed.append(sum_time_speed)
+       
+    summa_speed = sum(done_time_speed)
+    dlina_speed = len(done_time_speed)
+    average_speed = summa_speed / dlina_speed
+  
+    print(done_time_speed)
+    print(f'Ваше среднее время реакции ввода - {average_speed}')
+
+
+
+
+   
+
--- noMain.py
+++ noMain.py
@@ -1,1 +1,108 @@
-from game import guess_chislo
+from game import *
+import pyautogui as pag
+
+
+from tkinter import *
+window = Tk(className='Text helper')
+window.configure(bg='grey22')
+window.geometry('571x400')
+
+
+    
+
+def ygadai_chislo():
+    pag.moveTo(850,980)
+    pag.click(850,980,2,1,'left')
+    guess_chislo()
+def react_speed():
+    pag.moveTo(850,980)
+    pag.click(850,980,2,1,'left')
+    game_speed()
+def ymnay_game():
+    pag.moveTo(850,980)
+    pag.click(850,980,2,1,'left')
+    matematica_game()
+
+
+
+
+txt = Label(
+    text='Выберите действие',
+    width = 20,
+    height = 5,
+    font='arial,30',
+    bg='grey22',
+    fg ='white'
+)
+void1 = Label(
+    text=' ',
+    width = 1,
+    height = 10,
+    bg='grey22'
+)
+void2 = Label(
+    text=' ',
+    width = 30 ,
+    height = 10,
+    bg='grey22'
+)
+void3 = Label(
+    text=' ',
+    width = 30 ,
+    height = 10,
+    bg='grey22'
+)
+void4 = Label(
+    text=' ',
+    width = 30 ,
+    height = 10,
+    bg='grey22'
+)
+void5 = Label(
+    text=' ',
+    width = 30 ,
+    height = 10,
+    bg='grey22'
+)
+btn1 = Button( 
+    text = 'игра - "Угадай число"',
+    width = 20 ,
+    height = 5,
+    bg='grey26',
+    font='arial,16',
+    command=ygadai_chislo
+    )
+btn2 = Button( 
+    text = 'Игра на реакцию',
+    width = 20 ,
+    height = 5,
+    bg='grey26',
+    font='arial,16',
+    command=react_speed
+    )
+btn3 = Button( 
+    text = 'Математическая игра',
+    width = 20 ,
+    height = 5,
+    bg='grey26',
+    font='arial,16',
+    command=ymnay_game
+    )
+
+
+
+void1.grid(column=0,row=0)
+txt.grid(column=1,row=0)
+btn1.grid(column=0,row=1)
+btn2.grid(column=1,row=1)
+btn3.grid(column=2,row=1)
+
+
+
+
+
+
+
+
+
+window.mainloop()
